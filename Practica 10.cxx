b4d039a153d6d5e2090f2b9f1ae0c58883e6b3a0
kha73-code <karen74gh@gmail.com>
2025-03-20 14:01:17 -0600

    Add files via upload

---
+++ Practica 10.cxx
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main () {
+	double a = 7; 
+	double A = 3;
+	double B = 2;
+	
+	printf("%f", a *A *B);
+	printf("\n");
+	
+
+	double b = 4;
+	double c = 2;
+	printf("%f", b/c); 
+	printf("\n)");
+	return 0;
+	
+	
+	
+}
